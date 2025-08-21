c52cbb21823f68489f665741f8b4806ff934b7bd
DemonPlayer <DemonPlayer0@outlook.com>
2025-08-21 13:16:27 +0800

    修改数据库登录脚本防止密码泄漏

--- utils/database.js
+++ utils/database.js
@@ -1,11 +1,15 @@
 const mariaDB = require('mariadb');
+const path = require('path');
+const env = require('dotenv').config({ path: path.join(__dirname, '../.env') });
+
+console.log(process.env);
 
 // 创建连接池
 const pool = mariaDB.createPool({
-  host: 'localhost',
-  user: 'pixels_user', // 使用新创建的用户
-  password: 'NachoNO1',
-  database: 'Pixels',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   connectionLimit: 10, // 增加连接池大小
   connectTimeout: 5000, // 连接超时5秒
   acquireTimeout: 10000, // 获取连接超时10秒
